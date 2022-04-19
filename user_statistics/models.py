974d16a4a0ad0e62fdf53aa3d800d3c4bbf67cb0
Thomas Gamauf <thomas.gamauf@gmail.com>
2022-04-19 13:46:28 +0200

    Implement usage statistics
    * Implement statistics collection
    * Add schedules for statistics collection
    * Add schedule for cleanup of user visits
    * Upgrade black and prettier used by pre-commit

--- user_statistics/models.py
+++ user_statistics/models.py
@@ -1,57 +1,16 @@
 from __future__ import annotations
 from django.db import models
-from django.utils import timezone
-from typing import Union
 
 from user_visit.models import UserVisit
 
 
-class DailyUsers(models.Model):
-    date = models.DateField(auto_now_add=True)
-    day_of_month = models.PositiveIntegerField()
+class DailyActiveUsers(models.Model):
+    date = models.DateField(primary_key=True)
     count = models.PositiveIntegerField()
     fraction = models.DecimalField(max_digits=3, decimal_places=2)
 
-    def save(
-        self, force_insert=False, force_update=False, using=None, update_fields=None
-    ):
-        if not self.day_of_month:
-            self.day_of_month = self.date.isoweekday()
-        super().save(force_insert, force_update, using, update_fields)
 
-
-class MonthlyUsers(models.Model):
-    date = models.DateField(auto_now_add=True)
-    month = models.PositiveIntegerField()
+class MonthlyActiveUsers(models.Model):
+    date = models.DateField(primary_key=True)
     count = models.PositiveIntegerField()
     fraction = models.DecimalField(max_digits=3, decimal_places=2)
-
-    def save(
-        self, force_insert=False, force_update=False, using=None, update_fields=None
-    ):
-        if not self.month:
-            self.month = self.date.month
-        super().save(force_insert, force_update, using, update_fields)
-
-
-class UserVisitQueryset(models.QuerySet):
-    # The queryset is meant to be used on a UserVisit model in order to
-    #  aggregate the number of uses for the specified timeframe.
-
-    def __init__(self):
-        super().__init__(model=UserVisit)
-
-    def day(self) -> Union[UserVisitQueryset, models.QuerySet]:
-        # Get the daily users for the current day of the provided date.
-        now = timezone.now()
-        return self.filter(
-            timestamp__year=now.year, timestamp__month=now.month, timestamp__day=now.day
-        )
-
-    def month(self) -> Union[UserVisitQueryset, models.QuerySet]:
-        # Get the daily users for the month of the provided date.
-        now = timezone.now()
-        return self.filter(timestamp__year=now.year, timestamp__month=now.month)
-
-    def distinct_users(self) -> Union[UserVisitQueryset, models.QuerySet]:
-        return self.distinct("user")
--- user_statistics/services.py
+++ user_statistics/services.py
@@ -1,19 +1,62 @@
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
+from django.utils import timezone
+from user_visit.models import UserVisit
+
 from users.models import CustomUser
 
-from .models import DailyUsers, MonthlyUsers, UserVisitQueryset
+from .models import DailyActiveUsers, MonthlyActiveUsers
 
 
-def calculate_daily_users() -> None:
+def calculate_daily_active_users() -> None:
+    # Get the daily users for the previous day.
+
+    now = timezone.now()
+    target_date = now - relativedelta(days=1)
     count_users = CustomUser.objects.count()
-    count_users_day = UserVisitQueryset().day().distinct_users().count()
-    DailyUsers.objects.create(
-        count=count_users_day, fraction=float(count_users_day) / count_users
+    count_users_day = (
+        UserVisit.objects.filter(
+            timestamp__year=target_date.year,
+            timestamp__month=target_date.month,
+            timestamp__day=target_date.day,
+        )
+        .distinct("user")
+        .count()
+    )
+    DailyActiveUsers.objects.create(
+        date=target_date,
+        count=count_users_day,
+        fraction=float(count_users_day) / count_users,
     )
 
 
-def calculate_monthly_usage() -> None:
+def calculate_monthly_active_users() -> None:
+    # Get the daily users for the last month.
+    # The date will be the last day of the month.
+
+    now = timezone.now()
+    target_date = date(year=now.year, month=now.month, day=1) - relativedelta(days=1)
     count_users = CustomUser.objects.count()
-    count_users_month = UserVisitQueryset().day().distinct_users().count()
-    MonthlyUsers.objects.create(
-        count=count_users_month, fraction=float(count_users_month) / count_users
+    count_users_month = (
+        UserVisit.objects.filter(
+            timestamp__year=target_date.year, timestamp__month=target_date.month
+        )
+        .distinct("user")
+        .count()
     )
+    MonthlyActiveUsers.objects.create(
+        date=target_date,
+        count=count_users_month,
+        fraction=float(count_users_month) / count_users,
+    )
+
+
+def delete_past_user_visits() -> None:
+    # Delete all user visits of previous months to comply with the principle of
+    #  data economy.
+
+    now = timezone.now()
+    end_datetime = datetime(
+        year=now.year, month=now.month, day=1, hour=0, minute=0, second=0
+    )
+    UserVisit.objects.filter(timestamp__lt=end_datetime).delete()
