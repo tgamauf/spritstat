19ec683be7c09bfdb24f718c8f48c0fe5a616206
Thomas Gamauf <thomas.gamauf@gmail.com>
2022-04-29 17:02:07 +0200

    Upgrade to new react rendering api

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,20 +3,22 @@ import "intro.js/introjs.css";
 import "intro.js/themes/introjs-modern.css"
 
 import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 import App from "./app/App";
 import {store} from "./app/store";
 
-ReactDOM.render(
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App/>
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
