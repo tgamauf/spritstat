31c2587b41af012fe079663746b3f3abc79e4a7d
Thomas Gamauf <thomas.gamauf@farside.ai>
2022-12-18 23:26:10 +0100

    Additional migrations for Cypress 11

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -6,11 +6,6 @@ export default defineConfig({
   screenshotOnRunFailure: false,
   video: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
     baseUrl: 'http://127.0.0.1:8000',
   },
 })
