3e0bc0dc27f61c37445a538da2fc3a4b62bac816
Thomas Gamauf <thomas.gamauf@farside.ai>
2022-12-18 23:15:11 +0100

    Remove obsolete deployment directory

--- deployment/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# exit when any command fails
-set -e
-
-docker build -t ghcr.io/tgamauf/spritstat:latest -f docker/Dockerfile .
-docker push ghcr.io/tgamauf/spritstat:latest
-ansible-playbook --ask-vault-pass -i inventory -u ubuntu playbook.yml
